392cb0ead383b5263300eddadac8fd8d4ea5edf6
Reptilectric51 <al221910421@gmail.com>
2020-11-04 16:46:20 -0600

    Metodo all () en Laravel

--- Awos-Laravel-6/app/Http/Controllers/usercontroller.php
+++ Awos-Laravel-6/app/Http/Controllers/usercontroller.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
  use Illuminate\Http\Request;
  use App\Alumnos;
+ use APP\Alumnos as ProductCreateRequest;
 
  class UserController extends Controllers
  {
+public function __construct (Alumnos $Alumnos){
+    $this->Alumnos = Alumnos;
+}
+
      public function index()
      {       
             $alumnos          =   alumnos::all();
--- Awos-Laravel-6/app/Http/Requests/ProductCreateRequest.php
+++ Awos-Laravel-6/app/Http/Requests/ProductCreateRequest.php
@@ -24,7 +24,7 @@ class ProductCreateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'Nombre'=> 'required'
         ];
     }
 }
